743d71ad97d6ae5b7677d460d8c3e28ee918f479
Haikalfnda <haikalalfonda123@gmail.com>
2026-05-09 14:20:44 +0700

    5.Menyimpan preference layout

---
+++ app/src/main/java/com/haikal0045/kuliahku/util/SettingsDataStore.kt
@@ -0,0 +1,40 @@
+package com.haikal0045.kuliahku.util
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "settings_preference"
+)
+
+class SettingsDataStore(private val context: Context) {
+
+    private val showListKey = booleanPreferencesKey("show_list")
+
+    val layoutFlow: Flow<Boolean> = context.dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            preferences[showListKey] ?: true
+        }
+
+    suspend fun saveLayout(showList: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[showListKey] = showList
+        }
+    }
+}
